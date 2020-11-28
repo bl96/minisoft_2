24188381f0b39132b6adf04d9d0604af808b2a33
bl96 <47901930+bl96@users.noreply.github.com>
2020-11-28 17:58:01 +0100

    1.2.0

--- program.py
+++ program.py
@@ -9,6 +9,10 @@ def create_dictionary_for_images(path, image_list):
     return images
 
 
+def create_list_for_images(path, number_of_images):
+    return [tkinter.PhotoImage(file=f"{path}{i}.png") for i in range(0, number_of_images)]
+
+
 class Program:
     def __init__(self):
         win = tkinter.Tk()
@@ -19,11 +23,15 @@ class Program:
                                                                      ["close", "editor", "load", "start"])
         self.main_menu_buttons_filled = create_dictionary_for_images("textures/buttons/menu/filled/",
                                                                      ["close", "editor", "load", "start"])
-        self.hero_idle_right_pictures = [tkinter.PhotoImage(file=f"textures/hero/hero_idle_small/{i}.png") for i in
-                                         range(0, 10)]
+        self.hero_idle_pictures = {"r": create_list_for_images("textures/hero/hero_idle_right/", 11),
+                                   "l": create_list_for_images("textures/hero/hero_idle_left/", 11)
+                                   }
+        self.hero_move_pictures = {"r": create_list_for_images("textures/hero/hero_move_right/", 19),
+                                   "l": create_list_for_images("textures/hero/hero_move_left/", 19)
+                                   }
 
         self.canvas.pack()
-        MainMenu(self.canvas, self.main_menu_bg_pictures, self.hero_idle_right_pictures, self.main_menu_buttons_normal,
+        MainMenu(self.canvas, self.main_menu_bg_pictures, self.hero_idle_pictures, self.main_menu_buttons_normal,
                  self.main_menu_buttons_filled)
         win.mainloop()
 
@@ -40,7 +48,7 @@ class MainMenu:
         self.hero_idle_pictures = hero_idle_pictures
 
         self.background_image = self.canvas.create_image(480, 270, image=self.main_menu_bg_pictures)
-        self.hero_image = self.canvas.create_image(360, 480, image=self.hero_idle_pictures[0])
+        self.hero_image = self.canvas.create_image(360, 480, image=self.hero_idle_pictures["r"][0])
         self.main_menu_buttons_image = {}
 
         self.hero_image_counter = 0
@@ -52,21 +60,17 @@ class MainMenu:
         self.buttons_action_bind = self.canvas.tag_bind("button", "<Button-1>", self.buttons_action)
 
     def background_hero_animation(self):
-        self.canvas.itemconfig(self.hero_image, image=self.hero_idle_pictures[self.hero_image_counter])
+        self.canvas.itemconfig(self.hero_image, image=self.hero_idle_pictures["r"][self.hero_image_counter])
         self.hero_image_counter += 1
-        if self.hero_image_counter == len(self.hero_idle_pictures):
+        if self.hero_image_counter == len(self.hero_idle_pictures["r"]):
             self.hero_image_counter = 0
         self.canvas.after(300, self.background_hero_animation)
 
     def create_buttons(self):
         y = 50
         for buttonName in ["start", "load", "editor", "close"]:
-            self.main_menu_buttons_image[buttonName] = self.canvas.create_image(120, y,
-                                                                                image=self.main_menu_buttons_normal[
-                                                                                    buttonName],
-                                                                                tag="button")
+            self.main_menu_buttons_image[buttonName] = self.canvas.create_image(120, y, image=self.main_menu_buttons_normal[buttonName], tag="button")
             y += 40
-
         self.canvas.update()
 
     def filled_button(self, event):
